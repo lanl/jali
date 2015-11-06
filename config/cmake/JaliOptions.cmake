eebb55f4407f47ff64b786364080bbc4c85cd117
Rao Garimella <rao@lanl.gov>
2015-11-06 16:22:32 -0700

    add c++11 support for compilation

--- config/cmake/JaliOptions.cmake
+++ config/cmake/JaliOptions.cmake
@@ -17,6 +17,18 @@ enable_language(C)
 enable_language(CXX)
 enable_language(Fortran)
 
+# ENABLE C++11 support
+
+include(CheckCXXCompilerFlag)
+CHECK_CXX_COMPILER_FLAG("-std=c++11" COMPILER_SUPPORTS_CXX11)
+if(COMPILER_SUPPORTS_CXX11)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
+else()
+    message(FATAL_ERROR "Compiler ${CMAKE_CXX_COMPILER} has no C++11 support.")
+endif()
+
+
+
 # No idea why we need this.
 # I think it was required for Franklin build. -- lpritch
 if(PREFER_STATIC_LIBRARIES)
