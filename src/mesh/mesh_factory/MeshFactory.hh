1f0d3f4120a87bccd32e4290cfb52eb29ae714c9
Ondřej Čertík <ondrej.certik@gmail.com>
2016-02-10 12:58:10 -0700

    MeshFactory: Move std::unique_ptr one level up
    The create() functions now return raw pointers as before, but we wrap it into a
    unique_ptr for the operator(), which is the public API. To make this
    consistent, the create() functions are now made private, because public API
    should not use owning raw pointers.

--- src/mesh/mesh_factory/MeshFactory.hh
+++ src/mesh/mesh_factory/MeshFactory.hh
@@ -47,6 +47,51 @@ class MeshFactory {
   /// private, undefined copy constructor to avoid unwanted copies
   MeshFactory(MeshFactory& old);
 
+  /// Create a mesh by reading the specified file (or set of files)
+  Mesh *create(const std::string& filename, 
+               const JaliGeometry::GeometricModelPtr &gm = 
+               (JaliGeometry::GeometricModelPtr) NULL,
+               const bool request_faces = true,
+               const bool request_edges = false,
+               const bool request_wedges=false,
+               const bool request_corners=false);
+
+
+  /// Create a hexahedral mesh of the specified dimensions
+  Mesh *create(double x0, double y0, double z0,
+               double x1, double y1, double z1,
+               int nx, int ny, int nz, 
+               const JaliGeometry::GeometricModelPtr &gm = 
+               (JaliGeometry::GeometricModelPtr) NULL,
+               const bool request_faces = true,
+               const bool request_edges = false,
+               const bool request_wedges=false,
+               const bool request_corners=false);
+
+    
+  /// Create a quadrilateral mesh of the specified dimensions
+  Mesh *create(double x0, double y0,
+               double x1, double y1,
+               int nx, int ny,
+               const JaliGeometry::GeometricModelPtr &gm = 
+               (JaliGeometry::GeometricModelPtr) NULL,
+               const bool request_faces = true,
+               const bool request_edges = false,
+               const bool request_wedges=false,
+               const bool request_corners=false);
+
+    
+  /// Create a mesh by extract subsets of entities from an existing mesh
+  Mesh *create(const Mesh *inmesh,
+               const std::vector<std::string> setnames,
+               const Entity_kind setkind,
+               const bool flatten = false,
+               const bool extrude = false,
+               const bool request_faces = true,
+               const bool request_edges = false,
+               const bool request_wedges=false,
+               const bool request_corners=false);
+
  public:
 
   /// Default constructor.
@@ -62,52 +107,6 @@ class MeshFactory {
   /// Set the framework preference
   void preference(const FrameworkPreference& pref);
 
-  /// Create a mesh by reading the specified file (or set of files)
-  std::unique_ptr<Mesh> create(const std::string& filename, 
-               const JaliGeometry::GeometricModelPtr &gm = 
-               (JaliGeometry::GeometricModelPtr) NULL,
-               const bool request_faces = true,
-               const bool request_edges = false,
-               const bool request_wedges=false,
-               const bool request_corners=false);
-
-
-  /// Create a hexahedral mesh of the specified dimensions
-  std::unique_ptr<Mesh> create(double x0, double y0, double z0,
-               double x1, double y1, double z1,
-               int nx, int ny, int nz, 
-               const JaliGeometry::GeometricModelPtr &gm = 
-               (JaliGeometry::GeometricModelPtr) NULL,
-               const bool request_faces = true,
-               const bool request_edges = false,
-               const bool request_wedges=false,
-               const bool request_corners=false);
-
-    
-  /// Create a quadrilateral mesh of the specified dimensions
-  std::unique_ptr<Mesh> create(double x0, double y0,
-               double x1, double y1,
-               int nx, int ny,
-               const JaliGeometry::GeometricModelPtr &gm = 
-               (JaliGeometry::GeometricModelPtr) NULL,
-               const bool request_faces = true,
-               const bool request_edges = false,
-               const bool request_wedges=false,
-               const bool request_corners=false);
-
-    
-  /// Create a mesh by extract subsets of entities from an existing mesh
-  std::unique_ptr<Mesh> create(const Mesh *inmesh,
-               const std::vector<std::string> setnames,
-               const Entity_kind setkind,
-               const bool flatten = false,
-               const bool extrude = false,
-               const bool request_faces = true,
-               const bool request_edges = false,
-               const bool request_wedges=false,
-               const bool request_corners=false);
-
-
   /// Create a mesh by reading the specified file (or set of files) -- operator
   std::unique_ptr<Mesh> operator() (const std::string& filename, 
                     const JaliGeometry::GeometricModelPtr &gm = 
@@ -117,8 +116,8 @@ class MeshFactory {
                     const bool request_wedges=false,
                     const bool request_corners=false) {
     
-    return create(filename, gm, request_faces, request_edges, request_wedges,
-                  request_corners);
+    return std::unique_ptr<Mesh>(create(filename, gm, request_faces, request_edges, request_wedges,
+                  request_corners));
   }
   
   /// Create a hexahedral mesh of the specified dimensions -- operator
@@ -132,8 +131,8 @@ class MeshFactory {
                     const bool request_wedges=false,
                     const bool request_corners=false) {
     
-    return create(x0, y0, z0, x1, y1, z1, nx, ny, nz, gm, 
-                  request_faces, request_edges, request_wedges, request_corners);
+    return std::unique_ptr<Mesh>(create(x0, y0, z0, x1, y1, z1, nx, ny, nz, gm, 
+                  request_faces, request_edges, request_wedges, request_corners));
   }
 
   /// Create a quadrilateral mesh of the specified dimensions -- operator
@@ -147,8 +146,8 @@ class MeshFactory {
                     const bool request_wedges=false,
                     const bool request_corners=false)  {
  
-    return create(x0, y0, x1, y1, nx, ny, gm, request_faces, request_edges,
-                  request_wedges, request_corners);
+    return std::unique_ptr<Mesh>(create(x0, y0, x1, y1, nx, ny, gm, request_faces, request_edges,
+                  request_wedges, request_corners));
   }
 
   /// Create a mesh by extract subsets of entities from an existing mesh
@@ -162,8 +161,8 @@ class MeshFactory {
                     const bool request_wedges=false,
                     const bool request_corners=false) {
     
-    return create(inmesh, setnames, setkind, flatten, extrude,
-                  request_faces, request_edges, request_wedges, request_corners);
+    return std::unique_ptr<Mesh>(create(inmesh, setnames, setkind, flatten, extrude,
+                  request_faces, request_edges, request_wedges, request_corners));
   }
 
 };
