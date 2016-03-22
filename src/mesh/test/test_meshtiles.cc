cc4e0469a8851a16e74d20c12b27a2020566d48a
Rao Garimella <rao@lanl.gov>
2016-03-21 18:09:00 -0600

    Completely revamped to test for mesh tiles with halos

--- src/mesh/test/test_meshtiles.cc
+++ src/mesh/test/test_meshtiles.cc
@@ -63,18 +63,21 @@ TEST(MESH_TILES_MPI) {
 
     int ierr = 0;
     int aerr = 0;
-    int num_tiles = 8;  // number of tiles in mesh on each compute node
+    int num_tiles_requested = 8;  // number of tiles in mesh on each processor
     try {
       Jali::FrameworkPreference prefs(factory.preference());
       prefs.clear();
       prefs.push_back(the_framework);
       factory.preference(prefs);
 
-      // Create a mesh with tiles
+      // Create a mesh with tiles - RIGHT NOW, THIS IS RELYING ON THE
+      // FACT THAT THE DEFAULT NUMBER OF HALOS IS 1. IN THE NEXT
+      // ITERATION WE WILL MODIFY THE MESH FACTORY INTERFACE SO THAT
+      // IT TAKES THE NUMBER OF HALO LAYERS AS AN ARGUMENT
 
-      mesh = factory(-1.0, -1.0, -1.0, 1.0, 1.0, 1.0, 8, 8, 8, NULL,
+      mesh = factory(-1.0, -1.0, -1.0, 1.0, 1.0, 1.0, 4, 4, 4, NULL,
                      faces_requested, edges_requested,
-                     wedges_requested, corners_requested, num_tiles);
+                     wedges_requested, corners_requested, num_tiles_requested);
 
     } catch (const Jali::Message& e) {
       std::cerr << ": mesh error: " << e.what() << std::endl;
@@ -87,114 +90,158 @@ TEST(MESH_TILES_MPI) {
     MPI_Allreduce(&ierr, &aerr, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
     CHECK_EQUAL(aerr, 0);
 
-    std::vector<Jali::Entity_ID> allcells;
 
-    int ncells_on_proc = mesh->num_cells<Jali::Parallel_type::OWNED>();
-    int ncells_per_tile = ncells_on_proc/num_tiles;
+    auto const& meshtiles = mesh->tiles();
+    CHECK_EQUAL(num_tiles_requested, meshtiles.size());
+
+    // Check that each cell has one and only one owner
+
+    std::vector<int> cell_owner(mesh->num_cells(), -1);
+    std::vector<int> cell_num_owners(mesh->num_cells(), 0);
+    for (auto const& t : meshtiles) {
+      int tileID = t->ID();
+      for (auto const& c : t->cells<Jali::Parallel_type::OWNED>()) {
+        cell_owner[c] = tileID;
+        cell_num_owners[c]++;
+      }
+    }
+    for (auto const& c : mesh->cells()) {
+      CHECK(cell_owner[c] >= 0 && cell_owner[c] < num_tiles_requested);
+      CHECK_EQUAL(1, cell_num_owners[c]);
+    }
+
+    // Check that the master tile of a ghost cell really owns the cell
+      
+    for (auto const& t : meshtiles) {
+      int tileID = t->ID();
+      for (auto const& c : t->cells<Jali::Parallel_type::GHOST>())
+        CHECK_EQUAL(cell_owner[c], mesh->master_tile_ID_of_cell(c));
+    }
+
+    std::vector<int> node_owner(mesh->num_nodes(), -1);
+    std::vector<int> node_num_owners(mesh->num_nodes(), 0);
+    for (auto const& t : meshtiles) {
+      int tileID = t->ID();
+      for (auto const& n : t->nodes<Jali::Parallel_type::OWNED>()) {
+        node_owner[n] = tileID;
+        node_num_owners[n]++;
+      }
+    }
+    for (auto const& n : mesh->nodes()) {
+      CHECK(node_owner[n] >= 0 && node_owner[n] < num_tiles_requested);
+      CHECK_EQUAL(1, node_num_owners[n]);
+    }
+    for (auto const& t : meshtiles) {
+      int tileID = t->ID();
+      for (auto const& n : t->nodes<Jali::Parallel_type::GHOST>())
+        CHECK_EQUAL(node_owner[n], mesh->master_tile_ID_of_node(n));
+    }
+
+    // Check that each face has one and only one owner
+
+    std::vector<int> face_owner(mesh->num_faces(), -1);
+    std::vector<int> face_num_owners(mesh->num_faces(), 0);
+    for (auto const& t : meshtiles) {
+      int tileID = t->ID();
+      for (auto const& f : t->faces<Jali::Parallel_type::OWNED>()) {
+        face_owner[f] = tileID;
+        face_num_owners[f]++;
+      }
+    }
+    for (auto const& f : mesh->faces()) {
+      CHECK(face_owner[f] >= 0 && face_owner[f] < num_tiles_requested);
+      CHECK_EQUAL(1, face_num_owners[f]);
+    }
+
+    // Check that the master tile of a ghost face really owns the face
+      
+    for (auto const& t : meshtiles) {
+      int tileID = t->ID();
+      for (auto const& f : t->faces<Jali::Parallel_type::GHOST>())
+        CHECK_EQUAL(face_owner[f], mesh->master_tile_ID_of_face(f));
+    }
 
 
-    // Iterate through the tiles and retrieve info about them
+    // Check that each edge has one and only one owner
 
-    int ntiles = 0;
-    for (auto const & t : mesh->tiles()) {
-      ntiles++;
-
-      // Make sure we have the right number of cells (typically mesh
-      // tiles do not have any ghost cells)
-
-      int ncells_owned = t->num_cells<Jali::Parallel_type::OWNED>();
-      CHECK_EQUAL(ncells_per_tile, ncells_owned);
-      int ncells_ghost = t->num_cells<Jali::Parallel_type::GHOST>();
-      CHECK_EQUAL(0, ncells_ghost);
-
-      // Put them into a list making sure no duplicates were encountered
-
-      int ncells = 0;
-      for (auto c : t->cells<Jali::Parallel_type::OWNED>()) {
-        CHECK(std::find(allcells.begin(), allcells.end(), c) == allcells.end());
-        allcells.push_back(c);
-        ncells++;
+    if (edges_requested) {
+      std::vector<int> edge_owner(mesh->num_edges(), -1);
+      std::vector<int> edge_num_owners(mesh->num_edges(), 0);
+      for (auto const& t : meshtiles) {
+        int tileID = t->ID();
+        for (auto const& e : t->edges<Jali::Parallel_type::OWNED>()) {
+          edge_owner[e] = tileID;
+          edge_num_owners[e]++;
+        }
+      }
+      for (auto const& e : mesh->edges()) {
+        CHECK(edge_owner[e] >= 0 && edge_owner[e] < num_tiles_requested);
+        CHECK_EQUAL(1, edge_num_owners[e]);
       }
 
-      // check if we have the right number of nodes (each tile should
-      // have 4 cells in a square pattern which gives us 9 nodes -
-      // this will fail if the assumption of the tile structure is not
-      // true)
-
-      int nnodes_owned = t->num_nodes<Jali::Parallel_type::OWNED>();
-      int nnodes_ghost = t->num_nodes<Jali::Parallel_type::GHOST>();
-//      CHECK_EQUAL(9, nnodes_owned + nnodes_ghost);
-
-      // check that we can iterate through the nodes correctly
-
-      int nnodes = 0;
-      for (auto n : t->nodes<Jali::Parallel_type::OWNED>())
-        nnodes++;
-      for (auto n : t->nodes<Jali::Parallel_type::GHOST>())
-        nnodes++;
-//    CHECK_EQUAL(9, nnodes);
-      CHECK_EQUAL(nnodes_owned+nnodes_ghost, nnodes);
-
-      if (edges_requested) {
-        // Assuming the same cell pattern as before, we should have 12 edges
-        int nedges_owned = t->num_edges<Jali::Parallel_type::OWNED>();
-        int nedges_ghost = t->num_edges<Jali::Parallel_type::GHOST>();
-//        CHECK_EQUAL(12, nedges_owned + nedges_ghost);
-
-        // check that we can iterate through the edges correctly
-        int nedges = 0;
-        for (auto e : t->edges<Jali::Parallel_type::OWNED>())
-          nedges++;
-        for (auto e : t->edges<Jali::Parallel_type::GHOST>())
-          nedges++;
-//       CHECK_EQUAL(12, nedges);
-      }
-
-      if (faces_requested) {
-        // Assuming the same cell pattern as before, we should have 12 faces
-        int nfaces_owned = t->num_faces<Jali::Parallel_type::OWNED>();
-        int nfaces_ghost = t->num_faces<Jali::Parallel_type::GHOST>();
-//        CHECK_EQUAL(12, nfaces_owned + nfaces_ghost);
-
-        // check that we can iterate through the faces correctly
-        int nfaces = 0;
-        for (auto f : t->faces<Jali::Parallel_type::OWNED>())
-          nfaces++;
-        for (auto f : t->faces<Jali::Parallel_type::GHOST>())
-          nfaces++;
-//        CHECK_EQUAL(12, nfaces);
-        CHECK_EQUAL(nfaces_owned + nfaces_ghost, nfaces);
-      }
-
-      if (wedges_requested) {
-        // We should have 48 Parallel_type::OWNED wedges per cell
-
-        int nwedges = 0;
-        for (auto w : t->wedges<Jali::Parallel_type::OWNED>())
-          nwedges++;
-
-        CHECK_EQUAL(48*ncells_per_tile, nwedges);
-        int nwedges_owned = t->num_wedges<Jali::Parallel_type::OWNED>();
-        CHECK_EQUAL(nwedges, nwedges_owned);
-        int nwedges_ghost = t->num_wedges<Jali::Parallel_type::GHOST>();
-        CHECK_EQUAL(0, nwedges_ghost);
-      }
-
-      if (corners_requested) {
-        // We should have 8 corners per cell
-
-        int ncorners = 0;
-        for (auto cn : t->corners<Jali::Parallel_type::OWNED>())
-          ncorners++;
-
-        CHECK_EQUAL(8*ncells_per_tile, ncorners);
-        int ncorners_owned = t->num_corners<Jali::Parallel_type::OWNED>();
-        CHECK_EQUAL(ncorners, ncorners_owned);
-        int ncorners_ghost = t->num_corners<Jali::Parallel_type::GHOST>();
-        CHECK_EQUAL(0, ncorners_ghost);
+      // Check that the master tile of a ghost edge really owns the edge
+      
+      for (auto const& t : meshtiles) {
+        int tileID = t->ID();
+        for (auto const& e : t->edges<Jali::Parallel_type::GHOST>())
+          CHECK_EQUAL(edge_owner[e], mesh->master_tile_ID_of_edge(e));
       }
     }
 
-    CHECK_EQUAL(ncells_on_proc, allcells.size());
+
+    // Check that each wedge has one and only one owner
+
+    if (wedges_requested) {
+      std::vector<int> wedge_owner(mesh->num_wedges(), -1);
+      std::vector<int> wedge_num_owners(mesh->num_wedges(), 0);
+      for (auto const& t : meshtiles) {
+        int tileID = t->ID();
+        for (auto const& w : t->wedges<Jali::Parallel_type::OWNED>()) {
+          wedge_owner[w] = tileID;
+          wedge_num_owners[w]++;
+        }
+      }
+      for (auto const& w : mesh->wedges()) {
+        CHECK(wedge_owner[w] >= 0 && wedge_owner[w] < num_tiles_requested);
+        CHECK_EQUAL(1, wedge_num_owners[w]);
+      }
+
+      // Check that the master tile of a ghost wedge really owns the wedge
+      
+      for (auto const& t : meshtiles) {
+        int tileID = t->ID();
+        for (auto const& w : t->wedges<Jali::Parallel_type::GHOST>())
+          CHECK_EQUAL(wedge_owner[w], mesh->master_tile_ID_of_wedge(w));
+      }
+    }
+
+
+    if (corners_requested) {
+
+      // Check that each corner has one and only one owner
+
+      std::vector<int> corner_owner(mesh->num_corners(), -1);
+      std::vector<int> corner_num_owners(mesh->num_corners(), 0);
+      for (auto const& t : meshtiles) {
+        int tileID = t->ID();
+        for (auto const& cn : t->corners<Jali::Parallel_type::OWNED>()) {
+          corner_owner[cn] = tileID;
+          corner_num_owners[cn]++;
+        }
+      }
+      for (auto const& cn : mesh->corners()) {
+        CHECK(corner_owner[cn] >= 0 && corner_owner[cn] < num_tiles_requested);
+        CHECK_EQUAL(1, corner_num_owners[cn]);
+      }
+
+      // Check that the master tile of a ghost corner really owns the corner
+      
+      for (auto const& t : meshtiles) {
+        int tileID = t->ID();
+        for (auto const& cn : t->corners<Jali::Parallel_type::GHOST>())
+          CHECK_EQUAL(corner_owner[cn], mesh->master_tile_ID_of_corner(cn));
+      }
+    }
+
   }
 }
