87e51836b64e3bb061c1fda76c64bbf988565010
Rao Garimella <rao@lanl.gov>
2016-03-23 16:43:20 -0600

    had to revert the cast of identifier in std::to_string call to 'long long' from 'int64_t' since intel compiler said more than one function matched the signature

--- src/statemanager/JaliStateVector.h
+++ src/statemanager/JaliStateVector.h
@@ -62,7 +62,7 @@ class BaseStateVector {
   //! (which should be forbidden in user-defined names) to avoid
   //! potential collisions with string names if both are used.
   static std::string int_to_string(int const identifier) {
-    return ("~" + std::to_string(static_cast<int64_t>(identifier)));
+    return ("~" + std::to_string(static_cast<long long>(identifier)));
   }
 
   //! Query Metadata
