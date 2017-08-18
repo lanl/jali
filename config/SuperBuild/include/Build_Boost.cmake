b0c9e3cac990a03ff71b2e0e129631e86a2efd91
Eugene Kikinzon <kikinzon@lanl.gov>
2017-08-18 12:41:43 -0600

    Changes to building scripts to fix Boost and Unittest using default compilers instead of the ones specified by the user. On OSX, Boost is now compatible with Macports and Homebrew versions of gcc (and respective builds of MPI).

--- config/SuperBuild/include/Build_Boost.cmake
+++ config/SuperBuild/include/Build_Boost.cmake
@@ -1,44 +1,4 @@
-# Copyright (c) 2017, Los Alamos National Security, LLC
-# All rights reserved.
-
-# Copyright 2017. Los Alamos National Security, LLC. This software was
-# produced under U.S. Government contract DE-AC52-06NA25396 for Los
-# Alamos National Laboratory (LANL), which is operated by Los Alamos
-# National Security, LLC for the U.S. Department of Energy. The
-# U.S. Government has rights to use, reproduce, and distribute this
-# software.  NEITHER THE GOVERNMENT NOR LOS ALAMOS NATIONAL SECURITY,
-# LLC MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR ASSUMES ANY LIABILITY
-# FOR THE USE OF THIS SOFTWARE.  If software is modified to produce
-# derivative works, such modified software should be clearly marked, so
-# as not to confuse it with the version available from LANL.
- 
-# Additionally, redistribution and use in source and binary forms, with
-# or without modification, are permitted provided that the following
-# conditions are met:
-
-# 1.  Redistributions of source code must retain the above copyright
-# notice, this list of conditions and the following disclaimer.
-# 2.  Redistributions in binary form must reproduce the above copyright
-# notice, this list of conditions and the following disclaimer in the
-# documentation and/or other materials provided with the distribution.
-# 3.  Neither the name of Los Alamos National Security, LLC, Los Alamos
-# National Laboratory, LANL, the U.S. Government, nor the names of its
-# contributors may be used to endorse or promote products derived from
-# this software without specific prior written permission.
- 
-# THIS SOFTWARE IS PROVIDED BY LOS ALAMOS NATIONAL SECURITY, LLC AND
-# CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
-# BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
-# FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL LOS
-# ALAMOS NATIONAL SECURITY, LLC OR CONTRIBUTORS BE LIABLE FOR ANY
-# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
-# GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
-# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
-# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
+#  -*- mode: cmake -*-
 
 #
 # Build TPL: Boost 
@@ -55,15 +15,24 @@ Jali_tpl_version_write(FILENAME ${TPL_VERSIONS_INCLUDE_FILE}
 # -- Define build definitions
 
 # We only build what we need, this is NOT a full Boost install
-set(Boost_projects "system,filesystem,program_options,regex")
+set(Boost_projects "system,filesystem,program_options,regex,graph")
 
 # --- Define the configure command
+message("  etc: BOOST FLAGS ISSUES")
+set(Boost_bjam_args "cxxflags=${Jali_COMMON_CXXFLAGS}")
+string(REPLACE " " "\\ " Boost_bjam_args ${Boost_bjam_args})
+message("  etc: bjam args escape spaces not quote them: ${Boost_bjam_args}")
+
 
 # Determine toolset type
 set(Boost_toolset)
 string(TOLOWER ${CMAKE_C_COMPILER_ID} compiler_id_lc)
+
 if (compiler_id_lc)
   if (APPLE)
+    message(STATUS "BOOST: CMAKE_SYSTEM         = ${CMAKE_SYSTEM}")
+    message(STATUS "BOOST: CMAKE_SYSTEM_VERSION = ${CMAKE_SYSTEM_VERSION}")
+    message(STATUS "BOOST: compiler_id_lc       = ${compiler_id_lc}")
     # CMAKE_SYSTEM of the form Darwin-12.5.0
     # CMAKE_SYSTEM_VERSION is 12.5.0 corresponds to OSX 10.8.5
     STRING(REGEX REPLACE "\\..*" "" OS_VERSION_MAJOR ${CMAKE_SYSTEM_VERSION})
@@ -71,13 +40,13 @@ if (compiler_id_lc)
     if ( ${compiler_id_lc} STREQUAL "intel" )
       set(Boost_toolset intel-darwin)
     else()  
-      set(Boost_toolset darwin)
+      set(Boost_toolset clang)
     endif()  
     # some extra hints.
     if (${compiler_id_lc} STREQUAL "gnu")
       # On Mac OS 10.9, Clang has switched from using libstdc++ to libc++, so 
       # we need to tell it to do the opposite.
-      if ( ${OS_VERSION_MAJOR} GREATER 12 ) # OSX 10.9.x -> Darwin-13.x.y
+      if ( ${OS_VERSION_MAJOR} EQUAL 13 ) # OSX 10.9.x -> Darwin-13.x.y
         execute_process(COMMAND g++ -v ERROR_VARIABLE GXX_IS_CLANG)
         string(FIND ${GXX_IS_CLANG} "LLVM" LLVM_INDEX)
         if (NOT ${LLVM_INDEX} EQUAL -1)
@@ -86,7 +55,56 @@ if (compiler_id_lc)
           set(Boost_bjam_args "cxxflags=\"-stdlib=libstdc++\" linkflags=\"-stdlib=libstdc++\"")
         endif()
       endif()
+      # On Mac OS 10.10, we don't know what to do yet
+      if ( ${OS_VERSION_MAJOR} GREATER 13 ) # OSX 10.9.x -> Darwin-13.x.y
+        message (STATUS "BOOST: CMAKE_CXX_COMPILER   = ${CMAKE_CXX_COMPILER}")
+        # Check if it looks like an mpi wrapper
+	if ( CMAKE_CXX_COMPILER MATCHES "mpi" )
+          execute_process(
+            COMMAND ${CMAKE_CXX_COMPILER} -show
+            OUTPUT_VARIABLE  COMPILE_CMDLINE OUTPUT_STRIP_TRAILING_WHITESPACE
+            ERROR_VARIABLE   COMPILE_CMDLINE ERROR_STRIP_TRAILING_WHITESPACE
+            RESULT_VARIABLE  COMPILER_RETURN
+            )
+            # Extract the name of the compiler
+	    if ( COMPILER_RETURN EQUAL 0)
+	        string(REPLACE " " ";" COMPILE_CMDLINE_LIST ${COMPILE_CMDLINE})
+	        list(GET COMPILE_CMDLINE_LIST 0 RAW_CXX_COMPILER)
+		message (STATUS "BOOST: RAW_CXX_COMPILER     = ${RAW_CXX_COMPILER}")
+	    else()
+	        message (FATAL_ERROR "BOOST: Unable to determine the compiler command")
+            endif()
+	else()
+          set(RAW_CXX_COMPILER ${CMAKE_CXX_COMPILER})
+        endif()
+
+        # Extract the version of the compiler
+	execute_process(
+	  COMMAND ${RAW_CXX_COMPILER} --version 
+	  OUTPUT_VARIABLE  _version_string OUTPUT_STRIP_TRAILING_WHITESPACE
+	  ERROR_VARIABLE   _version_string_error ERROR_STRIP_TRAILING_WHITESPACE
+	  RESULT_VARIABLE  _version_return
+        )
+
+        # Test to see if it is macports or clang
+        if ( _version_string MATCHES "MacPorts" OR _version_string MATCHES "Homebrew")
+          message (STATUS "BOOST: compiler is MacPorts or Homebrew" )
+          message (STATUS "BOOST: compiler version     = ${CMAKE_CXX_COMPILER_VERSION}")
+          set(BOOST_user_jam "/Users/ftuser/user-config.jam")
+          set(BOOST_using "using gcc : ${CMAKE_CXX_COMPILER_VERSION} : ${RAW_CXX_COMPILER} \;")
+          message (STATUS "BOOST: ${BOOST_using}")
+	  message (STATUS "BOOST: Boost_build_dir = ${Boost_build_dir}" )
+          file (MAKE_DIRECTORY ${Boost_build_dir})
+	  file (WRITE ${Boost_build_dir}/user-config.jam ${BOOST_using} \n )
+          set(Boost_bootstrap_args )
+          set(Boost_bjam_args "toolset=gcc-${CMAKE_CXX_COMPILER_VERSION} link=static" )
+          set(Boost_toolset darwin)
+        elseif( _version_string MATCHES "LLVM" )
+	  message (STATUS "BOOST: compiler is Clang" )
+        endif()
+      endif()
     endif()
+
   elseif(UNIX)
     if ( ${compiler_id_lc} STREQUAL "gnu" )
         set(Boost_toolset gcc)
@@ -96,6 +114,8 @@ if (compiler_id_lc)
         set(Boost_toolset pgi)
     elseif(${compiler_id_lc} STREQUAL "pathscale")
         set(Boost_toolset pathscale)
+    elseif(${compiler_id_lc} STREQUAL "clang")
+        set(Boost_toolset clang)
     endif()
   endif()
 endif()
