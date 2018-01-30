59350c7b28a618b7888eeb09c5065330717d30ef
Rao Garimella <rao@lanl.gov>
2018-01-30 16:44:18 -0700

    brought TPLs in line with Amanzi TPLs

--- config/SuperBuild/TPLVersions.cmake
+++ config/SuperBuild/TPLVersions.cmake
@@ -39,6 +39,7 @@
 #   1.0.7       - MSTK updated to 2.27rc2
 #   1.0.8       - MSTK updated to 2.27rc3
 #   1.0.9       - MSTK updated to 2.27 and UnitTest++ updated to 1.6.0
+#   1.1.0       - MSTK updated to 3.0.3, UnitTest++ to 2.0.0, Trilinos to 12.10.1, HDF5 1.8.18, Seacas to #173a1e6, Xerces to 3.1.2, zlib to 1.2.11, Boost to 1.63.0, NetCDF to 4.4.1.1, MOAB to 5.0.0, 
 
 include(CMakeParseArguments)
 
@@ -85,10 +86,14 @@ macro(Jali_tpl_version_write)
 
 endmacro(Jali_tpl_version_write)
 
+#
+# Default location of TPLs on GitHub (poach Amanzi's TPL collection)
+#
+set (JALI_TPLS_DOWNLOAD_URL "https://raw.githubusercontent.com/amanzi/amanzi-tpls/master/src")
 
 set (JALI_TPLS_VERSION_MAJOR 1)
-set (JALI_TPLS_VERSION_MINOR 0)
-set (JALI_TPLS_VERSION_PATCH 2)
+set (JALI_TPLS_VERSION_MINOR 1)
+set (JALI_TPLS_VERSION_PATCH 0)
 set (JALI_TPLS_VERSION ${JALI_TPLS_VERSION}.${JALI_TPLS_VERSION_MINOR}.${JALI_TPLS_VERSION_PATCH})
 #   Not sure how to create a meaningful hash key for the collection
 
@@ -97,11 +102,11 @@ set (JALI_TPLS_VERSION ${JALI_TPLS_VERSION}.${JALI_TPLS_VERSION_MINOR}.${JALI_TP
 #
 set(XERCES_VERSION_MAJOR 3)
 set(XERCES_VERSION_MINOR 1)
-set(XERCES_VERSION_PATCH 1)
+set(XERCES_VERSION_PATCH 2)
 set(XERCES_VERSION ${XERCES_VERSION_MAJOR}.${XERCES_VERSION_MINOR}.${XERCES_VERSION_PATCH})
-set(XERCES_URL_STRING     "http://software.lanl.gov/ascem/tpls")
-set(XERCES_ARCHIVE_FILE   xerces-c-${XERCES_VERSION}.tar.gz)
-set(XERCES_MD5_SUM        6a8ec45d83c8cfb1584c5a5345cb51ae ) 
+set(XERCES_URL_STRING     ${JALI_TPLS_DOWNLOAD_URL})
+set(XERCES_ARCHIVE_FILE   xerces-c-${XERCES_VERSION}.tar.bz2)
+set(XERCES_MD5_SUM        d987b8bb576aea456e92454781fe3615 ) 
 
 #
 # TPL: OpenMPI
@@ -110,7 +115,7 @@ set(OpenMPI_VERSION_MAJOR 1)
 set(OpenMPI_VERSION_MINOR 4)
 set(OpenMPI_VERSION_PATCH 4)
 set(OpenMPI_VERSION ${OpenMPI_VERSION_MAJOR}.${OpenMPI_VERSION_MINOR}.${OpenMPI_VERSION_PATCH})
-set(OpenMPI_URL_STRING     "http://software.lanl.gov/ascem/tpls")
+set(OpenMPI_URL_STRING     ${JALI_TPLS_DOWNLOAD_URL})
 set(OpenMPI_ARCHIVE_FILE   openmpi-${OpenMPI_VERSION}.tar.bz2)
 set(OpenMPI_MD5_SUM        e58a1ea7b8af62453aaa0ddaee5f26a0) 
 
@@ -121,7 +126,7 @@ set(CURL_VERSION_MAJOR 7)
 set(CURL_VERSION_MINOR 37)
 set(CURL_VERSION_PATCH 0)
 set(CURL_VERSION ${CURL_VERSION_MAJOR}.${CURL_VERSION_MINOR}.${CURL_VERSION_PATCH})
-set(CURL_URL_STRING     "http://software.lanl.gov/ascem/tpls")
+set(CURL_URL_STRING     ${JALI_TPLS_DOWNLOAD_URL})
 set(CURL_ARCHIVE_FILE   curl-${CURL_VERSION}.tar.bz2)
 set(CURL_MD5_SUM        7dda0cc2e4136f78d5801ac347be696b)
 
@@ -130,11 +135,11 @@ set(CURL_MD5_SUM        7dda0cc2e4136f78d5801ac347be696b)
 #
 set(ZLIB_VERSION_MAJOR 1)
 set(ZLIB_VERSION_MINOR 2)
-set(ZLIB_VERSION_PATCH 6)
+set(ZLIB_VERSION_PATCH 11)
 set(ZLIB_VERSION ${ZLIB_VERSION_MAJOR}.${ZLIB_VERSION_MINOR}.${ZLIB_VERSION_PATCH})
-set(ZLIB_URL_STRING     "http://software.lanl.gov/ascem/tpls")
+set(ZLIB_URL_STRING     ${JALI_TPLS_DOWNLOAD_URL})
 set(ZLIB_ARCHIVE_FILE   zlib-${ZLIB_VERSION}.tar.gz)
-set(ZLIB_MD5_SUM        618e944d7c7cd6521551e30b32322f4a) 
+set(ZLIB_MD5_SUM        1c9f62f0778697a09d36121ead88e08e) 
 
 #
 # TPL: METIS
@@ -143,65 +148,54 @@ set(METIS_VERSION_MAJOR 5)
 set(METIS_VERSION_MINOR 1)
 set(METIS_VERSION_PATCH 0)
 set(METIS_VERSION ${METIS_VERSION_MAJOR}.${METIS_VERSION_MINOR}.${METIS_VERSION_PATCH})
-set(METIS_URL_STRING     "http://software.lanl.gov/ascem/tpls")
+set(METIS_URL_STRING     ${JALI_TPLS_DOWNLOAD_URL})
 set(METIS_ARCHIVE_FILE   metis-${METIS_VERSION}.tar.gz)
 set(METIS_MD5_SUM        5465e67079419a69e0116de24fce58fe)
 
 #
 # TPL: UnitTest
 #
-set(UnitTest_VERSION_MAJOR 1)
-set(UnitTest_VERSION_MINOR 6)
+set(UnitTest_VERSION_MAJOR 2)
+set(UnitTest_VERSION_MINOR 0)
 set(UnitTest_VERSION_PATCH 0)
 set(UnitTest_VERSION ${UnitTest_VERSION_MAJOR}.${UnitTest_VERSION_MINOR}.${UnitTest_VERSION_PATCH})
-set(UnitTest_URL_STRING     "http://software.lanl.gov/ascem/tpls")
-set(UnitTest_ARCHIVE_FILE   unittest-cpp-${UnitTest_VERSION}.zip)
-set(UnitTest_MD5_SUM       bca285acea948ab3cd76e8cb17f65d24) 
+set(UnitTest_URL_STRING     ${JALI_TPLS_DOWNLOAD_URL})
+set(UnitTest_ARCHIVE_FILE   unittest-cpp-${UnitTest_VERSION}.tgz)
+set(UnitTest_MD5_SUM       29f958e355e516e7ab016b467974728d)
 
 #
 # TPL: Boost
 #
 set(Boost_VERSION_MAJOR 1)
-set(Boost_VERSION_MINOR 58)
+set(Boost_VERSION_MINOR 63)
 set(Boost_VERSION_PATCH 0)
 set(Boost_VERSION        ${Boost_VERSION_MAJOR}.${Boost_VERSION_MINOR}.${Boost_VERSION_PATCH})
 set(Boost_VERSION_STRING ${Boost_VERSION_MAJOR}_${Boost_VERSION_MINOR}_${Boost_VERSION_PATCH})
-set(Boost_URL_STRING     "http://software.lanl.gov/ascem/tpls")
+set(Boost_URL_STRING     ${JALI_TPLS_DOWNLOAD_URL})
 set(Boost_ARCHIVE_FILE   boost_${Boost_VERSION_STRING}.tar.bz2)
-set(Boost_MD5_SUM        b8839650e61e9c1c0a89f371dd475546)
-
-#
-# TPL: BoostCmake
-#
-set(BoostCmake_VERSION_MAJOR 1)
-set(BoostCmake_VERSION_MINOR 46)
-set(BoostCmake_VERSION_PATCH 1)
-set(BoostCmake_VERSION        ${BoostCmake_VERSION_MAJOR}.${BoostCmake_VERSION_MINOR}.${BoostCmake_VERSION_PATCH})
-set(BoostCmake_URL_STRING     "http://software.lanl.gov/ascem/tpls")
-set(BoostCmake_ARCHIVE_FILE   boost-cmake-cmake-${BoostCmake_VERSION}.tar.gz)
-set(BoostCmake_MD5_SUM        ) 
+set(Boost_MD5_SUM        1c837ecd990bb022d07e7aab32b09847)
 
 #
 # TPL: HDF5
 #
 set(HDF5_VERSION_MAJOR 1)
 set(HDF5_VERSION_MINOR 8)
-set(HDF5_VERSION_PATCH 8)
+set(HDF5_VERSION_PATCH 18)
 set(HDF5_VERSION ${HDF5_VERSION_MAJOR}.${HDF5_VERSION_MINOR}.${HDF5_VERSION_PATCH})
-set(HDF5_URL_STRING    "http://software.lanl.gov/ascem/tpls")
+set(HDF5_URL_STRING     ${JALI_TPLS_DOWNLOAD_URL})
 set(HDF5_ARCHIVE_FILE   hdf5-${HDF5_VERSION}.tar.gz)
-set(HDF5_MD5_SUM        1196e668f5592bfb50d1de162eb16cff)      
+set(HDF5_MD5_SUM        dd2148b740713ca0295442ec683d7b1c)      
 
 #
 # TPL: NetCDF
 #
 set(NetCDF_VERSION_MAJOR 4)
-set(NetCDF_VERSION_MINOR 3)
-set(NetCDF_VERSION_PATCH 2)
+set(NetCDF_VERSION_MINOR 4)
+set(NetCDF_VERSION_PATCH 1.1)
 set(NetCDF_VERSION ${NetCDF_VERSION_MAJOR}.${NetCDF_VERSION_MINOR}.${NetCDF_VERSION_PATCH})
-set(NetCDF_URL_STRING     "http://software.lanl.gov/ascem/tpls")
+set(NetCDF_URL_STRING     ${JALI_TPLS_DOWNLOAD_URL})
 set(NetCDF_ARCHIVE_FILE   netcdf-${NetCDF_VERSION}.tar.gz)
-set(NetCDF_MD5_SUM        2fd2365e1fe9685368cd6ab0ada532a0)
+set(NetCDF_MD5_SUM        503a2d6b6035d116ed53b1d80c811bda)
 
 #
 # TPL: NetCDF Fortran
@@ -209,7 +203,7 @@ set(NetCDF_MD5_SUM        2fd2365e1fe9685368cd6ab0ada532a0)
 set(NetCDF_Fortran_VERSION_MAJOR 4)
 set(NetCDF_Fortran_VERSION_MINOR 2)
 set(NetCDF_Fortran_VERSION ${NetCDF_Fortran_VERSION_MAJOR}.${NetCDF_Fortran_VERSION_MINOR})
-set(NetCDF_Fortran_URL_STRING     "http://software.lanl.gov/ascem/tpls")
+set(NetCDF_Fortran_URL_STRING     ${JALI_TPLS_DOWNLOAD_URL})
 set(NetCDF_Fortran_ARCHIVE_FILE   netcdf-fortran-${NetCDF_Fortran_VERSION}.tar.gz)
 set(NetCDF_Fortran_MD5_SUM        cc3bf530223e8f4aff93793b9f197bf3) 
 
@@ -219,54 +213,54 @@ set(NetCDF_Fortran_MD5_SUM        cc3bf530223e8f4aff93793b9f197bf3)
 set(ExodusII_VERSION_MAJOR 6)
 set(ExodusII_VERSION_MINOR 06)
 set(ExodusII_VERSION ${ExodusII_VERSION_MAJOR}.${ExodusII_VERSION_MINOR})
-set(ExodusII_URL_STRING    "http://software.lanl.gov/ascem/tpls")
+set(ExodusII_URL_STRING    ${JALI_TPLS_DOWNLOAD_URL})
 set(ExodusII_ARCHIVE_FILE  exodus-${ExodusII_VERSION}.tar.gz)
 set(ExodusII_MD5_SUM       cfd240dbc1251b08fb1d0ee2de40a44c)
 
 #
 # TPL: MSTK
 #
-set(MSTK_VERSION_MAJOR 2)
-set(MSTK_VERSION_MINOR 27)
-set(MSTK_VERSION_PATCH )
+set(MSTK_VERSION_MAJOR 3)
+set(MSTK_VERSION_MINOR 0)
+set(MSTK_VERSION_PATCH 3)
 if (MSTK_VERSION_PATCH)
   set(MSTK_VERSION ${MSTK_VERSION_MAJOR}_${MSTK_VERSION_MINOR}_${MSTK_VERSION_PATCH})
 else (MSTK_VERSION_PATCH)
   set(MSTK_VERSION ${MSTK_VERSION_MAJOR}_${MSTK_VERSION_MINOR})
 endif (MSTK_VERSION_PATCH)
 set(MSTK_URL_STRING     "https://github.com/MeshToolkit/MSTK/archive")
-set(MSTK_ARCHIVE_FILE   MSTK-${MSTK_VERSION}.tar.gz)
-set(MSTK_MD5_SUM       d14a28ff45d4c13790ea066524c7fafa)
+set(MSTK_ARCHIVE_FILE   v${MSTK_VERSION}.tar.gz)
+set(MSTK_MD5_SUM        a9561f1175da2c2863c517b08abe5f16)
 
 #
 # TPL: MOAB
 #
-set(MOAB_VERSION_MAJOR  r4276)
-set(MOAB_VERSION_MINOR  )
-set(MOAB_VERSION_PATCH  )
-set(MOAB_URL_STRING     "http://software.lanl.gov/ascem/tpls")
+
+set(MOAB_VERSION_MAJOR  5)
+set(MOAB_VERSION_MINOR  0)
+set(MOAB_VERSION_PATCH  0)
+set(MOAB_URL_STRING     "ftp://ftp.mcs.anl.gov/pub/fathom")
 set(MOAB_ARCHIVE_FILE   MOAB-${MOAB_VERSION}.tar.gz)
-set(MOAB_MD5_SUM        49da04e8905f6d730d92521e7ca7400e) 
+set(MOAB_MD5_SUM        1840ca02366f4d3237d44af63e239e3b)
 
 #
 # TPL: Trilinos
 #
 set(Trilinos_VERSION_MAJOR 12)
-set(Trilinos_VERSION_MINOR 2)
-set(Trilinos_VERSION_PATCH 2)
-set(Trilinos_VERSION ${Trilinos_VERSION_MAJOR}.${Trilinos_VERSION_MINOR}.${Trilinos_VERSION_PATCH})
-set(Trilinos_URL_STRING     "http://software.lanl.gov/ascem/tpls")
-set(Trilinos_ARCHIVE_FILE   trilinos-${Trilinos_VERSION}-Source.tar.bz2)
-set(Trilinos_MD5_SUM        90bbb175505b55878401a51a1d17463c)
+set(Trilinos_VERSION_MINOR 10)
+set(Trilinos_VERSION_PATCH 1)
+set(Trilinos_VERSION ${Trilinos_VERSION_MAJOR}-${Trilinos_VERSION_MINOR}-${Trilinos_VERSION_PATCH})
+set(Trilinos_URL_STRING      "https://github.com/trilinos/Trilinos/archive")
+set(Trilinos_ARCHIVE_FILE   trilinos-release-${Trilinos_VERSION}.tar.gz)
+set(Trilinos_MD5_SUM        667333dbd7c0f031d47d7c5511fd0810)
 
 #
 # TPL: SEACAS
-#  SEACAS is available in Trilinos 10.8 and above
-set(SEACAS_VERSION_MAJOR 12)
-set(SEACAS_VERSION_MINOR 2)
-set(SEACAS_VERSION_PATCH 2)
-set(SEACAS_VERSION ${SEACAS_VERSION_MAJOR}.${SEACAS_VERSION_MINOR}.${SEACAS_VERSION_PATCH})
-set(SEACAS_URL_STRING     "http://software.lanl.gov/ascem/tpls")
-set(SEACAS_ARCHIVE_FILE   trilinos-${SEACAS_VERSION}-Source.tar.bz2)
-set(SEACAS_MD5_SUM        90bbb175505b55878401a51a1d17463c)
+set(SEACAS_VERSION_MAJOR 173a1e6)
+set(SEACAS_VERSION_MINOR 0)
+set(SEACAS_VERSION_PATCH 0)
+set(SEACAS_VERSION ${SEACAS_VERSION_MAJOR})
+set(SEACAS_URL_STRING     ${JALI_TPLS_DOWNLOAD_URL})
+set(SEACAS_ARCHIVE_FILE   seacas-${SEACAS_VERSION}.tgz)
+set(SEACAS_MD5_SUM        3235d1b885ee8e1a04408382f50bd0f0)
 
