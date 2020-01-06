b6f2cb01c30d9a8cdff019d8d78e18b65419ecc0
Rao Garimella <rao@abyzou.lanl.gov>
2020-01-05 22:17:28 -0700

    replaced with more up-to-date versions

--- config/cmake/Modules/FindExodusII.cmake
+++ config/cmake/Modules/FindExodusII.cmake
@@ -1,292 +1,125 @@
-# Copyright (c) 2019, Triad National Security, LLC
-# All rights reserved.
-
-# Copyright 2019. Triad National Security, LLC. This software was
-# produced under U.S. Government contract 89233218CNA000001 for Los
-# Alamos National Laboratory (LANL), which is operated by Triad
-# National Security, LLC for the U.S. Department of Energy. 
-# All rights in the program are reserved by Triad National Security,
-# LLC, and the U.S. Department of Energy/National Nuclear Security
-# Administration. The Government is granted for itself and others acting
-# on its behalf a nonexclusive, paid-up, irrevocable worldwide license
-# in this material to reproduce, prepare derivative works, distribute
-# copies to the public, perform publicly and display publicly, and to
-# permit others to do so
- 
-# 
-# This is open source software distributed under the 3-clause BSD license.
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are
-# met:
-# 
-# 1. Redistributions of source code must retain the above copyright notice,
-#    this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-# 3. Neither the name of Triad National Security, LLC, Los Alamos
-#    National Laboratory, LANL, the U.S. Government, nor the names of its
-#    contributors may be used to endorse or promote products derived from this
-#    software without specific prior written permission.
-# 
-#  
-# THIS SOFTWARE IS PROVIDED BY TRIAD NATIONAL SECURITY, LLC AND
-# CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
-# BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
-# FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
-# TRIAD NATIONAL SECURITY, LLC OR CONTRIBUTORS BE LIABLE FOR ANY
-# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
-# GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
-# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
-# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-
+# Copyright: 2019- Triad National Security, LLC
 #
-# Jali ExodusII Find Module
+# ExodusII Find Module for Jali
 #
-# Usage:
-#    Control the search through ExodusII_DIR or setting environment variable
-#    ExodusII_ROOT to the ExodusII installation prefix.
+# ExodusII needs METIS; This module will try to find METIS as well and add it
+# as a dependency
 #
-#    This module does not search default paths! 
+# Usage: To search a particular path you can specify the path in
+# CMAKE_PREFIX_PATH, in the CMake variable ExodusII_DIR or environment
+# variable ExodusII_ROOT
 #
-#    Following variables are set:
-#    ExodusII_FOUND            (BOOL)       Flag indicating if ExodusII was found
-#    ExodusII_INCLUDE_DIR      (PATH)       Path to the ExodusII include file
-#    ExodusII_INCLUDE_DIRS     (LIST)       List of all required include files
-#    ExodusII_LIBRARY_DIR      (PATH)       Path to the ExodusII library
-#    ExodusII_LIBRARY          (FILE)       ExodusII library
-#    ExodusII_LIBRARIES        (LIST)       List of all required ExodusII libraries
+# Following variables are set:
+# ExodusII_FOUND          (BOOL)   Flag indicating if ExodusII was found
+# ExodusII_INCLUDE_DIRS   (PATH)   Path to ExodusII include files
+# ExodusII_LIBRARY        (FILE)   ExodusII library (libzoltan.a, libzoltan.so)
+# ExodusII_LIBRARIES      (LIST)   List of ExodusII targets (ExodusII::ExodusII)
 #
-#    Additional variables
-#    ExodusII_VERSION          (STRING)     ExodusII Version string
+#
+# Additional variables
+# ExodusII_VERSION          (STRING)     ExodusII Version string
 #
 # #############################################################################
 
-# Standard CMake modules see CMAKE_ROOT/Modules
+# First use pkg-config to parse an installed .pc file to find the
+# library although we cannot rely on it
+
+find_package(PkgConfig)
+pkg_check_modules(PC_ExodusII QUIET exodusII)
+
+
+# Search for include files
+find_path(ExodusII_INCLUDE_DIR
+  NAMES exodusII.h
+  HINTS ${PC_ExodusII_INCLUDE_DIRS}
+  PATHS ${ExodusII_DIR}
+  PATH_SUFFIXES include
+  )
+
+if (NOT ExodusII_INCLUDE_DIR)
+  if (ExodusII_FIND_REQUIRED)
+    message(FATAL "Cannot locate exodusII.h")
+  else ()
+    if (NOT ExodusII_FIND_QUIETLY)
+      message(WARNING "Cannot locate exodusII.h")
+    endif ()
+  endif ()
+endif ()
+
+set(ExodusII_INCLUDE_DIRS "${ExodusII_INCLUDE_DIR}")
+
+
+# Search for libraries
+
+find_library(ExodusII_LIBRARY
+  NAMES exodus exoIIv2c
+  HINTS ${PC_ExodusII_LIBRARY_DIRS}
+  PATHS ${ExodusII_DIR}
+  PATH_SUFFIXES lib lib64)
+
+if (NOT ExodusII_LIBRARY)
+  if (ExodusII_FIND_REQUIRED)
+    message(FATAL "Can not locate ExodusII library")
+  else (ExodusII_FIND_REQUIRED)
+    if (NOT ExodusII_FIND_QUIETLY)
+      message(WARNING "Cannot locate ExodusII library")
+    endif ()
+  endif ()
+endif ()
+
+# Set library version
+
+set(ExodusII_VERSION ${PC_ExodusII_VERSION})  # No guarantee
+if (NOT ExodusII_VERSION AND ExodusII_INCLUDE_DIR)
+  set(exodus_h "${ExodusII_INCLUDE_DIR}/exodusII.h")
+  file(STRINGS "${exodus_h}" exodus_version_string REGEX "^#define EX_API_VERS")
+  string(REGEX REPLACE "^#define EX_API_VERS ([0-9]+\\.[0-9]+).*$" "\\1" exodus_version "${exodus_version_string}")
+
+  set(ExodusII_VERSION "${exodus_version}")
+endif ()
+
+# Finish setting standard variables if everything is found
 include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(ExodusII
+  DEFAULT_MSG
+  ExodusII_LIBRARY ExodusII_INCLUDE_DIR)
 
-include(AddImportedLibrary)
+# find_package_handle_standard_args ignores case and sets PACKAGE_FOUND
+if (NOT ExodusII_FOUND AND EXODUSII_FOUND)
+  set(ExodusII_FOUND ${EXODUSII_FOUND})
+endif ()
 
-# Jali CMake functions see <root>/tools/cmake for source
-include(PrintVariable)
-include(AddPackageDependency)
+# Create ExodusII target
 
-if ( ExodusII_LIBRARIES AND ExodusII_INCLUDE_DIRS )
+if (ExodusII_FOUND AND NOT TARGET ExodusII::ExodusII)
+  set(ExodusII_LIBRARIES ExodusII::ExodusII)
+  add_library(${ExodusII_LIBRARIES} UNKNOWN IMPORTED)
+  set_target_properties(${ExodusII_LIBRARIES} PROPERTIES
+    IMPORTED_LOCATION "${ExodusII_LIBRARY}"
+    INTERFACE_COMPILE_OPTIONS "${PC_ExodusII_CFLAGS_OTHER}"
+    INTERFACE_INCLUDE_DIRECTORIES "${ExodusII_INCLUDE_DIR}")
 
-    # Do nothing. Variables are set. No need to search again
-
-else(ExodusII_LIBRARIES AND ExodusII_INCLUDE_DIRS)
-
-    # Cache variables
-    if(ExodusII_DIR)
-        set(ExodusII_DIR "${ExodusII_DIR}" CACHE PATH "Path to search for ExodusII include and library files")
-    endif()
-
-    if(ExodusII_INCLUDE_DIR)
-        set(ExodusII_INCLUDE_DIR "${ExodusII_INCLUDE_DIR}" CACHE PATH "Path to search for ExodusII include files")
-    endif()
-
-    if(ExodusII_LIBRARY_DIR)
-        set(ExodusII_LIBRARY_DIR "${ExodusII_LIBRARY_DIR}" CACHE PATH "Path to search for ExodusII library files")
-    endif()
-
-    
-    # Search for include files
-    # Search order preference:
-    #  (1) ExodusII_INCLUDE_DIR - check existence of path AND if the include files exist
-    #  (2) ExodusII_DIR/<include>
-    #  (3) Default CMake paths See cmake --html-help=out.html file for more information.
-    #
-    set(exodus_inc_names "exodusII.h")
-    if (ExodusII_INCLUDE_DIR)
-
-        if (EXISTS "${ExodusII_INCLUDE_DIR}")
-
-            find_path(exodusII_test_include_path
-                      NAMES ${exodus_inc_names}
-                      HINTS ${ExodusII_INCLUDE_DIR}
-                      NO_DEFAULT_PATH)
-            if(NOT exodusII_test_include_path)
-                message(SEND_ERROR "Can not locate ${exodus_inc_names} in ${ExodusII_INCLUDE_DIR}")
-            endif()
-            set(ExodusII_INCLUDE_DIR "${exodusII_test_include_path}")
-
-        else()
-            message(SEND_ERROR "ExodusII_INCLUDE_DIR=${ExodusII_INCLUDE_DIR} does not exist")
-            set(ExodusII_INCLUDE_DIR "ExodusII_INCLUDE_DIR-NOTFOUND")
-        endif()
-
-   else() 
-
-        set(exodus_inc_suffixes "include")
-        if(ExodusII_DIR)
-
-            if (EXISTS "${ExodusII_DIR}" )
-
-                find_path(ExodusII_INCLUDE_DIR
-                          NAMES ${exodus_inc_names}
-                          HINTS ${ExodusII_DIR}
-                          PATH_SUFFIXES ${exodus_inc_suffixes}
-                          NO_DEFAULT_PATH)
-
-            else()
-                 message(SEND_ERROR "ExodusII_DIR=${ExodusII_DIR} does not exist")
-                 set(ExodusII_INCLUDE_DIR "ExodusII_INCLUDE_DIR-NOTFOUND")
-            endif()    
-
-
-        else()
-
-            find_path(ExodusII_INCLUDE_DIR
-                      NAMES ${exodus_inc_names}
-                      PATH_SUFFIXES ${exodus_inc_suffixes})
-
-        endif()
-
-    endif()
-
-    if ( NOT ExodusII_INCLUDE_DIR )
-        message(SEND_ERROR "Can not locate ExodusII include directory")
-    endif()
-
-    # Search for libraries 
-    # Search order preference:
-    #  (1) ExodusII_LIBRARY_DIR - check existence of path AND if the library file exists
-    #  (2) ExodusII_DIR/<lib,Lib>
-    #  (3) Default CMake paths See cmake --html-help=out.html file for more information.
-    #
-    set(exodus_lib_names "exoIIv2c" "exodus")
-    if (ExodusII_LIBRARY_DIR)
-
-        if (EXISTS "${ExodusII_LIBRARY_DIR}")
-
-            find_library(_ExodusII_LIBRARY
-                         NAMES ${exodus_lib_names}
-                         HINTS ${ExodusII_LIBRARY_DIR}
-                         NO_DEFAULT_PATH)
-
-            find_library(_ExodusII_Fortran_LIBRARY
-                         NAMES exodus_for
-                         HINTS ${ExodusII_LIBRARY_DIR}
-                         NO_DEFAULT_PATH)
-
-        else()
-            message(SEND_ERROR "ExodusII_LIBRARY_DIR=${ExodusII_LIBRARY_DIR} does not exist")
-            set(_ExodusII_LIBRARY "ExodusII_LIBRARY-NOTFOUND")
-            set(_ExodusII_Fortran_LIBRARY "ExodusII_Fortran_LIBRARY-NOTFOUND")
-        endif()
-
-    else() 
-
-        list(APPEND exodus_lib_suffixes "lib" "Lib")
-        if(ExodusII_DIR)
-
-            if (EXISTS "${ExodusII_DIR}" )
-
-                find_library(_ExodusII_LIBRARY
-                             NAMES ${exodus_lib_names}
-                             HINTS ${ExodusII_DIR}
-                             PATH_SUFFIXES ${exodus_lib_suffixes}
-                             NO_DEFAULT_PATH)
-                
-		find_library(_ExodusII_Fortran_LIBRARY
-                             NAMES exodus_for
-                             HINTS ${ExodusII_DIR}
-                             PATH_SUFFIXES ${exodus_lib_suffixes}
-                             NO_DEFAULT_PATH)
-
-            else()
-                 message(SEND_ERROR "ExodusII_DIR=${ExodusII_DIR} does not exist")
-                 set(ExodusII_LIBRARY "ExodusII_LIBRARY-NOTFOUND")
-                 set(ExodusII_Fortran_LIBRARY "ExodusII_Fortran_LIBRARY-NOTFOUND")
-            endif()    
-
-
-        else()
-
-            find_library(_ExodusII_LIBRARY
-                         NAMES ${exodus_lib_names}
-                         PATH_SUFFIXES ${exodus_lib_suffixes})
-
-            find_library(_ExodusII_Fortran_LIBRARY
-                         NAMES exodus_for
-                         PATH_SUFFIXES ${exodus_lib_suffixes})
-
-        endif()
-
-    endif()
-
-    # Create the library target store the name in ExodusII_LIBRARY
-    if ( _ExodusII_LIBRARY )
-        set(ExodusII_LIBRARY exodusii)
-        add_imported_library(${ExodusII_LIBRARY}
-                     LOCATION ${_ExodusII_LIBRARY}
-                     LINK_LANGUAGES "C;CXX")
-    else()
-        message(SEND_ERROR "Can not locate ExodusII library")
-    endif()
-
-    if ( _ExodusII_Fortran_LIBRARY )
-        set(ExodusII_Fortran_LIBRARY exodusii_for)
-        add_imported_library(${ExodusII_Fortran_LIBRARY}
-                             LOCATION ${_ExodusII_Fortran_LIBRARY}
-                             LINK_LANGUAGES "Fortran")
-        set_target_properties(${ExodusII_Fortran_LIBRARY} PROPERTIES
-	                      IMPORTED_LINK_INTERFACE_LIBRARIES "${Exodus_LIBRARY}")
-    endif()
-
-
-   
-    # Define prerequisite packages
-    set(ExodusII_INCLUDE_DIRS ${ExodusII_INCLUDE_DIR})
-    set(ExodusII_LIBRARIES    ${ExodusII_LIBRARY})
-
-    # Search for NetCDF
-    find_package(NetCDF QUIET REQUIRED)
-    set_target_properties(${ExodusII_LIBRARY} PROPERTIES
-                          IMPORTED_LINK_INTERFACE_LIBRARIES "${NetCDF_C_LIBRARIES}")
-    list(APPEND ExodusII_INCLUDE_DIRS ${NetCDF_INCLUDE_DIRS})
-
-    #add_package_dependency(ExodusII DEPENDS_ON NetCDF)
-
-   
-endif(ExodusII_LIBRARIES AND ExodusII_INCLUDE_DIRS )    
-
-# Define the version
-if ( ExodusII_INCLUDE_DIR )
-    set(exodus_h ${ExodusII_INCLUDE_DIR}/exodusII.h)
-    file(STRINGS ${exodus_h} exodus_version_string REGEX "^#define EX_API_VERS")
-    string(REGEX REPLACE "^#define EX_API_VERS ([0-9]+\\.[0-9]+).*$" "\\1" exodus_version "${exodus_version_string}")
-
-    #PRINT_VARIABLE(exodus_version_string)
-    #PRINT_VARIABLE(exodus_version)
-
-    set(ExodusII_VERSION "${exodus_version}")
-
-endif()    
-
-# Send useful message if everything is found
-find_package_handle_standard_args(ExodusII DEFAULT_MSG
-                                           ExodusII_INCLUDE_DIR
-                                           ExodusII_LIBRARIES)
-
-# find_package)handle)standard_args should set ExodusII_FOUND but it does not!
-if ( ExodusII_LIBRARIES AND ExodusII_INCLUDE_DIRS)
-    set(ExodusII_FOUND TRUE)
-else()
-    set(ExodusII_FOUND FALSE)
+  # ExodusII depends on netCDF. Attempt to find it
+  
+  # Try to discover through cmake config file (usually named
+  # netcdf-config.cmake netCDFConfig.cmake)
+  find_package(netCDF NAMES netcdf netCDF CONFIG HINTS ${netCDF_DIR})
+  if (NOT netCDF_FOUND)
+    # Fallback to Jali module named FindNetCDF.cmake
+    find_package(netCDF QUIET REQUIRED MODULE)
+  endif ()
+  
+  # Add netCDF as a dependency of ExodusII
+  target_link_libraries(${ExodusII_LIBRARIES} INTERFACE ${netCDF_LIBRARIES})
 endif()
 
-
+  
+# Hide these variables from the cache
 mark_as_advanced(
-  ExodusII_VERSION
   ExodusII_INCLUDE_DIR
   ExodusII_INCLUDE_DIRS
   ExodusII_LIBRARY
   ExodusII_LIBRARIES
-  ExodusII_LIBRARY_DIR
-)
+  ExodusII_VERSION
+  )
+
--- config/cmake/Modules/FindMETIS.cmake
+++ config/cmake/Modules/FindMETIS.cmake
@@ -1,231 +1,91 @@
-# Copyright (c) 2019, Triad National Security, LLC
-# All rights reserved.
-
-# Copyright 2019. Triad National Security, LLC. This software was
-# produced under U.S. Government contract 89233218CNA000001 for Los
-# Alamos National Laboratory (LANL), which is operated by Triad
-# National Security, LLC for the U.S. Department of Energy. 
-# All rights in the program are reserved by Triad National Security,
-# LLC, and the U.S. Department of Energy/National Nuclear Security
-# Administration. The Government is granted for itself and others acting
-# on its behalf a nonexclusive, paid-up, irrevocable worldwide license
-# in this material to reproduce, prepare derivative works, distribute
-# copies to the public, perform publicly and display publicly, and to
-# permit others to do so
- 
-# 
-# This is open source software distributed under the 3-clause BSD license.
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are
-# met:
-# 
-# 1. Redistributions of source code must retain the above copyright notice,
-#    this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-# 3. Neither the name of Triad National Security, LLC, Los Alamos
-#    National Laboratory, LANL, the U.S. Government, nor the names of its
-#    contributors may be used to endorse or promote products derived from this
-#    software without specific prior written permission.
-# 
-#  
-# THIS SOFTWARE IS PROVIDED BY TRIAD NATIONAL SECURITY, LLC AND
-# CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
-# BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
-# FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
-# TRIAD NATIONAL SECURITY, LLC OR CONTRIBUTORS BE LIABLE FOR ANY
-# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
-# GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
-# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
-# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-
+# Copyright: 2019- Triad National Security, LLC
 #
-# Jali METIS Find Module
+# METIS Find Module for Jali
 #
-# Usage:
-#    Control the search through METIS_DIR or setting environment variable
-#    METIS_ROOT to the METIS installation prefix.
+# Usage: To search a particular path you can specify the path in
+# CMAKE_PREFIX_PATH, in the CMake variable METIS_DIR or environment
+# variable METIS_ROOT
 #
-#    This module does not search default paths! 
-#
-#    Following variables are set:
-#    METIS_FOUND            (BOOL)       Flag indicating if METIS was found
-#    METIS_INCLUDE_DIR      (PATH)       Path to the METIS include file
-#    METIS_INCLUDE_DIRS     (LIST)       List of all required include files
-#    METIS_LIBRARY_DIR      (PATH)       Path to the METIS library
-#    METIS_LIBRARY          (FILE)       METIS library
-#    METIS_LIBRARIES        (LIST)       List of all required METIS libraries
+# Following variables are set:
+# METIS_FOUND          (BOOL)   Flag indicating if METIS was found
+# METIS_INCLUDE_DIRS   (PATH)   Path to METIS include files
+# METIS_LIBRARY        (FILE)   METIS library (libzoltan.a, libzoltan.so)
+# METIS_LIBRARIES      (LIST)   List of METIS targets (METIS::METIS)
 #
 # #############################################################################
 
-# Standard CMake modules see CMAKE_ROOT/Modules
+# First use pkg-config to parse an installed .pc file to find the
+# library although we cannot rely on it
+
+find_package(PkgConfig)
+pkg_check_modules(PC_METIS QUIET metis)
+
+
+# Search for include files
+
+find_path(METIS_INCLUDE_DIR
+  NAMES metis.h
+  HINTS ${PC_METIS_INCLUDE_DIRS}
+  PATHS ${METIS_DIR}
+  PATH_SUFFIXES include)
+
+if (NOT METIS_INCLUDE_DIR)
+  if (METIS_FIND_REQUIRED)
+    message(FATAL "Cannot locate metis.h")
+  else (METIS_FIND_REQUIRED)
+    if (NOT METIS_FIND_QUIETLY)
+      message(WARNING "Cannot locate metis.h")
+    endif ()
+  endif ()
+endif ()
+
+set(METIS_INCLUDE_DIRS "${METIS_INCLUDE_DIR}")
+
+
+# Search for libraries
+
+find_library(METIS_LIBRARY
+  NAMES metis
+  HINTS ${PC_METIS_LIBRARY_DIRS}
+  PATHS ${METIS_DIR}
+  PATH_SUFFIXES lib lib64)
+
+if (NOT METIS_LIBRARY)
+  if (METIS_FIND_REQUIRED)
+    message(FATAL "Can not locate METIS library")
+  else (METIS_FIND_REQUIRED)
+    if (NOT METIS_FIND_QUIETLY)
+      message(WARNING "Cannot locate METIS library")
+    endif ()
+  endif ()
+endif ()
+
+set(METIS_VERSION PC_METIS_VERSION})  # No guarantee
+
+
+# Finish setting standard variables if everything is found
 include(FindPackageHandleStandardArgs)
-
-# Jali CMake functions see <root>/tools/cmake for source
-include(PrintVariable)
-
-if ( METIS_LIBRARIES AND METIS_INCLUDE_DIRS )
-
-    # Do nothing. Variables are set. No need to search again
-
-else(METIS_LIBRARIES AND METIS_INCLUDE_DIRS)
-
-    # Cache variables
-    if(METIS_DIR)
-        set(METIS_DIR "${METIS_DIR}" CACHE PATH "Path to search for METIS include and library files")
-    endif()
-
-    if(METIS_INCLUDE_DIR)
-        set(METIS_INCLUDE_DIR "${METIS_INCLUDE_DIR}" CACHE PATH "Path to search for METIS include files")
-    endif()
-
-    if(METIS_LIBRARY_DIR)
-        set(METIS_LIBRARY_DIR "${METIS_LIBRARY_DIR}" CACHE PATH "Path to search for METIS library files")
-    endif()
-
-    
-    # Search for include files
-    # Search order preference:
-    #  (1) METIS_INCLUDE_DIR - check existence of path AND if the include files exist
-    #  (2) METIS_DIR/<include>
-    #  (3) Default CMake paths See cmake --html-help=out.html file for more information.
-    #
-    set(metis_inc_names "metis.h")
-    if (METIS_INCLUDE_DIR)
-
-        if (EXISTS "${METIS_INCLUDE_DIR}")
-
-            find_path(metis_test_include_path
-                      NAMES ${metis_inc_names}
-                      HINTS ${METIS_INCLUDE_DIR}
-                      NO_DEFAULT_PATH)
-            if(NOT metis_test_include_path)
-                message(SEND_ERROR "Can not locate ${metis_inc_names} in ${METIS_INCLUDE_DIR}")
-            endif()
-            set(METIS_INCLUDE_DIR "${metis_test_include_path}")
-
-        else()
-            message(SEND_ERROR "METIS_INCLUDE_DIR=${METIS_INCLUDE_DIR} does not exist")
-            set(METIS_INCLUDE_DIR "METIS_INCLUDE_DIR-NOTFOUND")
-        endif()
-
-   else() 
-
-        set(metis_inc_suffixes "include")
-        if(METIS_DIR)
-
-            if (EXISTS "${METIS_DIR}" )
-
-                find_path(METIS_INCLUDE_DIR
-                          NAMES ${metis_inc_names}
-                          HINTS ${METIS_DIR}
-                          PATH_SUFFIXES ${metis_inc_suffixes}
-                          NO_DEFAULT_PATH)
-
-            else()
-                 message(SEND_ERROR "METIS_DIR=${METIS_DIR} does not exist")
-                 set(METIS_INCLUDE_DIR "METIS_INCLUDE_DIR-NOTFOUND")
-            endif()    
+find_package_handle_standard_args(METIS
+  DEFAULT_MSG
+  METIS_LIBRARY METIS_INCLUDE_DIR)
 
 
-        else()
+# Create METIS target
 
-            find_path(METIS_INCLUDE_DIR
-                      NAMES ${metis_inc_names}
-                      PATH_SUFFIXES ${metis_inc_suffixes})
-
-        endif()
-
-    endif()
-
-    if ( NOT METIS_INCLUDE_DIR )
-        message(SEND_ERROR "Can not locate METIS include directory")
-    endif()
-
-    # Search for libraries 
-    # Search order preference:
-    #  (1) METIS_LIBRARY_DIR - check existence of path AND if the library file exists
-    #  (2) METIS_DIR/<lib,Lib>
-    #  (3) Default CMake paths See cmake --html-help=out.html file for more information.
-    #
-    set(metis_lib_names "metis")
-    if (METIS_LIBRARY_DIR)
-
-        if (EXISTS "${METIS_LIBRARY_DIR}")
-
-            find_library(METIS_LIBRARY
-                         NAMES ${metis_lib_names}
-                         HINTS ${METIS_LIBRARY_DIR}
-                         NO_DEFAULT_PATH)
-        else()
-            message(SEND_ERROR "METIS_LIBRARY_DIR=${METIS_LIBRARY_DIR} does not exist")
-            set(METIS_LIBRARY "METIS_LIBRARY-NOTFOUND")
-        endif()
-
-    else() 
-
-        list(APPEND metis_lib_suffixes "lib" "Lib")
-        if(METIS_DIR)
-
-            if (EXISTS "${METIS_DIR}" )
-
-                find_library(METIS_LIBRARY
-                             NAMES ${metis_lib_names}
-                             HINTS ${METIS_DIR}
-                             PATH_SUFFIXES ${metis_lib_suffixes}
-                             NO_DEFAULT_PATH)
-
-            else()
-                 message(SEND_ERROR "METIS_DIR=${METIS_DIR} does not exist")
-                 set(METISLIBRARY "METIS_LIBRARY-NOTFOUND")
-            endif()    
+if (METIS_FOUND AND NOT TARGET METIS::METIS)
+  set(METIS_LIBRARIES METIS::METIS)
+  add_library(${METIS_LIBRARIES} UNKNOWN IMPORTED)
+  set_target_properties(${METIS_LIBRARIES} PROPERTIES
+    IMPORTED_LOCATION "${METIS_LIBRARY}"
+    INTERFACE_COMPILE_OPTIONS "${PC_METIS_CFLAGS_OTHER}"
+    INTERFACE_INCLUDE_DIRECTORIES "${METIS_INCLUDE_DIR}")
+endif ()
 
 
-        else()
-
-            find_library(METIS_LIBRARY
-                         NAMES ${metis_lib_names}
-                         PATH_SUFFIXES ${metis_lib_suffixes})
-
-        endif()
-
-    endif()
-
-    if ( NOT METIS_LIBRARY )
-        message(SEND_ERROR "Can not locate METIS library")
-    endif()    
-
-   
-    # Define prerequisite packages
-    set(METIS_INCLUDE_DIRS ${METIS_INCLUDE_DIR})
-    set(METIS_LIBRARIES    ${METIS_LIBRARY})
-
-   
-endif(METIS_LIBRARIES AND METIS_INCLUDE_DIRS )    
-
-# Send useful message if everything is found
-find_package_handle_standard_args(METIS DEFAULT_MSG
-                                  METIS_LIBRARIES
-                                  METIS_INCLUDE_DIRS)
-
-# find_package_handle_standard_args should set METIS_FOUND but it does not!
-if ( METIS_LIBRARIES AND METIS_INCLUDE_DIRS)
-    set(METIS_FOUND TRUE)
-else()
-    set(METIS_FOUND FALSE)
-endif()
-
-# Define the version
-
+# Hide these variables from the cache
 mark_as_advanced(
   METIS_INCLUDE_DIR
   METIS_INCLUDE_DIRS
   METIS_LIBRARY
   METIS_LIBRARIES
-  METIS_LIBRARY_DIR
-)
+  )
