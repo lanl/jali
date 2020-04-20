07b9799f1c07e1d1d61c0564a2152cdeb52bf347
Rao Garimella <rao@lanl.gov>
2020-04-19 21:27:38 -0600

    Do not install Epetra

--- config/SuperBuild/include/Build_Trilinos.cmake
+++ config/SuperBuild/include/Build_Trilinos.cmake
@@ -64,18 +64,7 @@ Jali_tpl_version_write(FILENAME ${TPL_VERSIONS_INCLUDE_FILE}
   
 # --- Define the configuration parameters   
 
-#  - Trilinos Package Configuration
-
-#if(Trilinos_Build_Config_File)
-#  message(STATUS "Including Trilinos build configuration file ${Trilinos_Build_Config_File}")
-#  if ( NOT EXISTS ${Trilinos_Build_Config_File} )
-#    message(FATAL_ERROR "File ${Trilinos_Build_Config_File} does not exist.")
-#  endif()
-#  include(${Trilinos_Build_Config_File})
-#endif()
-
 # List of packages enabled in the Trilinos build
-set(Trilinos_PACKAGE_LIST Epetra)
 #if ( ENABLE_STK_Mesh )
 #  list(APPEND Trilinos_PACKAGE_LIST STK)
 #endif()
@@ -90,10 +79,6 @@ set(Trilinos_CMAKE_PACKAGE_ARGS "-DTrilinos_ENABLE_ALL_PACKAGES:BOOL=OFF")
 foreach(package ${Trilinos_PACKAGE_LIST})
   list(APPEND Trilinos_CMAKE_PACKAGE_ARGS "-DTrilinos_ENABLE_${package}:STRING=ON")
 endforeach()
-
-# Trilinos 11.0.3 has some C++ compile errors in it that we can sidestep by 
-# defining HAVE_TEUCHOS_ARRAY_BOUNDSCHECK.
-#list(APPEND Trilinos_CMAKE_PACKAGE_ARGS "-DTeuchos_ENABLE_ABC:BOOL=ON")
 
 # Remove SEACAS from the build and force STK to use external Exodus
 if ( ENABLE_STK_Mesh )
